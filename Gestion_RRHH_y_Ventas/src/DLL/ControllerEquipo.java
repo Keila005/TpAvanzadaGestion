4ec504b5536909891942270b06187c57c6823b27
V-underEevee <vunder.mos@gmail.com>
2026-06-01 01:44:34 -0300

    Update ControllerEquipo.java

--- Gestion_RRHH_y_Ventas/src/DLL/ControllerEquipo.java
+++ Gestion_RRHH_y_Ventas/src/DLL/ControllerEquipo.java
@@ -2,117 +2,166 @@ package DLL;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.time.LocalDate;
 import java.util.LinkedList;
 
 import com.mysql.jdbc.Connection;
 
-import LogicLayer.ComentarioAnonimo;
-import LogicLayer.Empleado;
 import LogicLayer.Equipo;
 
-
 public class ControllerEquipo {
-	private static Connection con = Conexion.getInstance().getConnection();
-	
-	public LinkedList<Equipo> mostrarEquipo() {
 
-	    LinkedList<Equipo> equipo = new LinkedList<>();
-	    try {
-	    	  PreparedStatement stmt = con.prepareStatement
-	    			  ("SELECT `id_equipo`,`nombre` FROM `equipo`");
+    private static Connection con =
+            Conexion.getInstance().getConnection();
 
-		        ResultSet rs = stmt.executeQuery();
+    // MOSTRAR TODOS LOS EQUIPOS
+    public LinkedList<Equipo> mostrarEquipo() {
 
-	            while (rs.next()) {
-	            	
-	            	int id = rs.getInt("id_equipo");
-	                String nombre = rs.getString("nombre");
- 
-	                equipo.add(new Equipo(id,nombre));
-				}
-		} catch (Exception e) {
-			 e.printStackTrace();
-		}
-	    return equipo;
-	}
-	
-	public void agregarMiembro(int idOperativo, int idEquipo) {
+        LinkedList<Equipo> equipo = new LinkedList<>();
 
-	    try {
+        try {
 
-	        PreparedStatement stmt = con.prepareStatement(
+            PreparedStatement stmt =
+                    con.prepareStatement(
 
-	            "INSERT INTO equipo_miembro "
-	            + "(id_operativo, id_equipo) "
-	            + "VALUES (?, ?)"
-	        );
+                    "SELECT id_equipo,nombre "
+                    + "FROM equipo"
+            );
 
-	        stmt.setInt(1, idOperativo);
-	        stmt.setInt(2, idEquipo);
+            ResultSet rs = stmt.executeQuery();
 
-	        stmt.executeUpdate();
+            while(rs.next()) {
 
-	    } catch(Exception e) {
+                int id = rs.getInt("id_equipo");
+                String nombre = rs.getString("nombre");
 
-	        e.printStackTrace();
-	    }
-	}
-	
-	public void crearEquipo(
-	        String nombre,
-	        int idLider) {
+                equipo.add(
+                        new Equipo(id,nombre)
+                );
+            }
 
-	    try {
+        } catch(Exception e) {
 
-	        PreparedStatement stmt =
-	                con.prepareStatement(
+            e.printStackTrace();
+        }
 
-	            "INSERT INTO equipo "
-	            + "(nombre,id_lider) "
-	            + "VALUES (?,?)"
-	        );
+        return equipo;
+    }
 
-	        stmt.setString(1, nombre);
-	        stmt.setInt(2, idLider);
+    // CREAR EQUIPO NUEVO
+    public void crearEquipo(
+            String nombre,
+            int idLider) {
 
-	        stmt.executeUpdate();
+        try {
 
-	    } catch(Exception e) {
+            PreparedStatement stmt =
+                    con.prepareStatement(
 
-	        e.printStackTrace();
-	    }
-	}
-	
-	public int obtenerUltimoEquipo() {
+                    "INSERT INTO equipo "
+                    + "(nombre,id_lider) "
+                    + "VALUES (?,?)"
+            );
 
-	    int id = 0;
+            stmt.setString(1, nombre);
+            stmt.setInt(2, idLider);
 
-	    try {
+            stmt.executeUpdate();
 
-	        PreparedStatement stmt =
-	                con.prepareStatement(
+        } catch(Exception e) {
 
-	            "SELECT MAX(id_equipo) AS id "
-	            + "FROM equipo"
-	        );
+            e.printStackTrace();
+        }
+    }
 
-	        ResultSet rs = stmt.executeQuery();
+    // OBTENER ID DEL ULTIMO EQUIPO CREADO
+    public int obtenerUltimoEquipo() {
 
-	        if(rs.next()) {
+        int id = 0;
 
-	            id = rs.getInt("id");
-	        }
+        try {
 
-	    } catch(Exception e) {
+            PreparedStatement stmt =
+                    con.prepareStatement(
 
-	        e.printStackTrace();
-	    }
+                    "SELECT MAX(id_equipo) AS id "
+                    + "FROM equipo"
+            );
 
-	    return id;
-	}
-	
-	
-	
-	
+            ResultSet rs = stmt.executeQuery();
+
+            if(rs.next()) {
+
+                id = rs.getInt("id");
+            }
+
+        } catch(Exception e) {
+
+            e.printStackTrace();
+        }
+
+        return id;
+    }
+
+    // AGREGAR MIEMBRO A UN EQUIPO
+    public void agregarMiembro(
+            int idOperativo,
+            int idEquipo) {
+
+        try {
+
+            PreparedStatement stmt =
+                    con.prepareStatement(
+
+                    "INSERT INTO equipo_miembro "
+                    + "(id_operativo,id_equipo) "
+                    + "VALUES (?,?)"
+            );
+
+            stmt.setInt(1, idOperativo);
+            stmt.setInt(2, idEquipo);
+
+            stmt.executeUpdate();
+
+        } catch(Exception e) {
+
+            e.printStackTrace();
+        }
+    }
+
+    // OBTENER MIEMBROS DE UN EQUIPO
+    public LinkedList<Integer> obtenerMiembros(
+            int idEquipo) {
+
+        LinkedList<Integer> miembros =
+                new LinkedList<>();
+
+        try {
+
+            PreparedStatement stmt =
+                    con.prepareStatement(
+
+                    "SELECT id_operativo "
+                    + "FROM equipo_miembro "
+                    + "WHERE id_equipo = ?"
+            );
+
+            stmt.setInt(1, idEquipo);
+
+            ResultSet rs = stmt.executeQuery();
+
+            while(rs.next()) {
+
+                miembros.add(
+                        rs.getInt("id_operativo")
+                );
+            }
+
+        } catch(Exception e) {
+
+            e.printStackTrace();
+        }
+
+        return miembros;
+    }
+
 }
