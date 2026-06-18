78f42f869d26e0e38d56d1efe504d7487b8f8977
V-underEevee <vunder.mos@gmail.com>
2026-06-18 17:35:29 -0300

    TAL CUAAAA;L

--- Gestion_RRHH_y_Ventas/UserLayer/MenuOperativo.java
+++ Gestion_RRHH_y_Ventas/UserLayer/MenuOperativo.java
@@ -9,6 +9,10 @@ import javax.swing.JTextPane;
 import javax.swing.JTextField;
 import javax.swing.JLabel;
 import java.awt.Font;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.Color;
 
 public class MenuOperativo extends JFrame {
 
@@ -46,6 +50,35 @@ public class MenuOperativo extends JFrame {
 		lblNewLabel.setFont(new Font("Verdana", Font.BOLD, 16));
 		lblNewLabel.setBounds(30, 10, 377, 44);
 		contentPane.add(lblNewLabel);
+		
+		JButton btnNewButton = new JButton("Ver informacion personal");
+		btnNewButton.setBackground(new Color(172, 238, 123));
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnNewButton.setBounds(10, 64, 200, 44);
+		contentPane.add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("Solicitar vacaciones/permisos");
+		btnNewButton_1.setBounds(10, 129, 214, 44);
+		contentPane.add(btnNewButton_1);
+		
+		JButton btnNewButton_2 = new JButton("Comentar");
+		btnNewButton_2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnNewButton_2.setBounds(249, 129, 177, 44);
+		contentPane.add(btnNewButton_2);
+		
+		JButton btnNewButton_3 = new JButton("Cargo");
+		btnNewButton_3.setBounds(226, 64, 200, 44);
+		contentPane.add(btnNewButton_3);
+		
+		JButton btnNewButton_4 = new JButton("Salir");
+		btnNewButton_4.setBounds(325, 211, 101, 31);
+		contentPane.add(btnNewButton_4);
 
 	}
 }
