b4e69761e8eb5f01fe86b208f9b197c9bbcfb7a5
V-underEevee <vunder.mos@gmail.com>
2026-06-17 22:21:46 -0300

    Update Lider.java

--- Gestion_RRHH_y_Ventas/LogicLayer/Lider.java
+++ Gestion_RRHH_y_Ventas/LogicLayer/Lider.java
@@ -5,11 +5,19 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import javax.swing.JPasswordField;
+import javax.swing.JTextPane;
+import javax.swing.JSpinner;
+import javax.swing.JScrollBar;
+import javax.swing.JToggleButton;
+import javax.swing.JTextArea;
+import java.awt.Font;
 
 public class Lider extends JFrame {
 
 	private static final long serialVersionUID = 1L;
-	private JPanel contentPane;
 
 	/**
 	 * Launch the application.
@@ -32,10 +40,28 @@ public class Lider extends JFrame {
 	 */
 	public Lider() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
-		contentPane = new JPanel();
+		setBounds(100, 100, 636, 429);
+		JPanel contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JButton btnNewButton_2 = new JButton("New button");
+		btnNewButton_2.setBounds(90, 203, 85, 21);
+		contentPane.add(btnNewButton_2);
+		
+		JButton btnNewButton_1 = new JButton("New button");
+		btnNewButton_1.setBounds(196, 203, 85, 21);
+		contentPane.add(btnNewButton_1);
+		
+		JButton btnNewButton = new JButton("New button");
+		btnNewButton.setBounds(306, 203, 85, 21);
+		contentPane.add(btnNewButton);
+		
+		JLabel lblNewLabel = new JLabel("Menú Lider");
+		lblNewLabel.setFont(new Font("Verdana", Font.BOLD, 20));
+		lblNewLabel.setBounds(253, 23, 276, 53);
+		contentPane.add(lblNewLabel);
 
 	}
 
