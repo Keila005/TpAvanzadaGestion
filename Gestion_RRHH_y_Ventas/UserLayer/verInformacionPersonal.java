acf8492695f2779e4abfa052aa57a7a55c8d3a99
V-underEevee <vunder.mos@gmail.com>
2026-06-20 15:39:37 -0300

    info personal

--- Gestion_RRHH_y_Ventas/UserLayer/verInformacionPersonal.java
+++ Gestion_RRHH_y_Ventas/UserLayer/verInformacionPersonal.java
@@ -10,6 +10,9 @@ import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.JLabel;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JButton;
 import java.awt.Color;
 
@@ -45,7 +48,38 @@ public class verInformacionPersonal extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
+		JButton btnNewButton = new JButton("Ver mi sueldo");
+		btnNewButton.setFont(new Font("Tahoma", Font.BOLD, 12));
+		btnNewButton.setBackground(new Color(172, 238, 123));
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnNewButton.setBounds(10, 64, 200, 44);
+		contentPane.add(btnNewButton);
 		
+		JButton btnNewButton_1 = new JButton("Rendimiento");
+		btnNewButton_1.setFont(new Font("Tahoma", Font.BOLD, 12));
+		btnNewButton_1.setBounds(10, 129, 214, 44);
+		contentPane.add(btnNewButton_1);
+		
+		JButton btnNewButton_2 = new JButton("Mis datos");
+		btnNewButton_2.setFont(new Font("Tahoma", Font.BOLD, 12));
+		btnNewButton_2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnNewButton_2.setBounds(249, 129, 177, 44);
+		contentPane.add(btnNewButton_2);
+		
+		JButton btnNewButton_3 = new JButton("Ver ausencias");
+		btnNewButton_3.setFont(new Font("Tahoma", Font.BOLD, 12));
+		btnNewButton_3.setBounds(226, 64, 200, 44);
+		contentPane.add(btnNewButton_3);
+		
+		JButton btnNewButton_4 = new JButton("Volver");
+		btnNewButton_4.setBounds(325, 211, 101, 31);
+		contentPane.add(btnNewButton_4);
 
 	}
 }
