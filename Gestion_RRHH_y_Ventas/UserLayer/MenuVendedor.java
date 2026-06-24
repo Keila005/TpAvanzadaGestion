a91131f7f7a532019412737599f4f304b2542d22
enni <enith.valer@davinci.edu.ar>
2026-06-23 23:43:47 -0300

    menu principal de vendedor

--- Gestion_RRHH_y_Ventas/UserLayer/MenuVendedor.java
+++ Gestion_RRHH_y_Ventas/UserLayer/MenuVendedor.java
@@ -8,6 +8,10 @@ import LogicLayer.Usuario;
 
 import javax.swing.JLabel;
 import java.awt.Font;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.Color;
 
 public class MenuVendedor extends JFrame {
 
@@ -38,11 +42,43 @@ public class MenuVendedor extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JLabel lblNewLabel = new JLabel("Bienvenido vendedor:"+ usuario.getNombre());
-		lblNewLabel.setBounds(100, 11, 159, 21);
+		JLabel lblNewLabel = new JLabel("Bienvenido vendedor: <dynamic>");
+		lblNewLabel.setBounds(31, 50, 271, 21);
 		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 17));
 		contentPane.add(lblNewLabel);
+		
+		JButton btnNewButton = new JButton("Stock");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnNewButton.setBounds(31, 116, 190, 51);
+		contentPane.add(btnNewButton);
+		
+		JButton btnPerfilLaboral = new JButton("Perfil Laboral");
+		btnPerfilLaboral.setBounds(31, 210, 190, 51);
+		contentPane.add(btnPerfilLaboral);
+		
+		JButton btnVenta = new JButton("Venta");
+		btnVenta.setBounds(269, 116, 190, 51);
+		contentPane.add(btnVenta);
+		
+		JButton btnHistorialDeVentas = new JButton("Historial de Ventas");
+		btnHistorialDeVentas.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnHistorialDeVentas.setBounds(269, 210, 190, 51);
+		contentPane.add(btnHistorialDeVentas);
+		
+		JButton btnCerrarSession = new JButton("Cerrar Session");
+		btnCerrarSession.setForeground(new Color(255, 0, 0));
+		btnCerrarSession.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnCerrarSession.setBounds(326, 45, 133, 36);
+		contentPane.add(btnCerrarSession);
 
 	}
-
 }
